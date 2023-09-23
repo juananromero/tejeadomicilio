db6c834d821052067039baef3e763e482a5b3a31
juananromero <aromero@uco.es>
2023-09-23 09:24:30 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 print("Hola Tejedoras")
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
